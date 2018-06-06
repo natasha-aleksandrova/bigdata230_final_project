8970f586387ce30db8af2a1a3c417f6e3bc8e7b8
Natasha <natasha.aleksandrova@zonarsystems.com>
2018-06-06 14:37:24 -0700

    Update seat_geek_ingest.py

--- seat_geek_ingest.py
+++ seat_geek_ingest.py
@@ -6,42 +6,37 @@ import os
 
 es = Elasticsearch()
 
-mappings ={    'mappings': {
-    "seat_geek": {
-                 "properties": {
-                     "location": {
-                         "type": "geo_point"
-                     }
-                 }
-        }
-    }
-    }
+mappings = {
+	'mappings': {
+		"seat_geek": {
+			"properties": {
+				"location": {
+					"type": "geo_point"
+				}
+			}
+		}
+	}
+}
+
+
 try:
     es.indices.create(index='sg2', body=mappings)
 except Exception as e:
     print(e)
 
-# ...
-# es_entries['geo'] = { 'location': str(data['_longitude_'])+","+str(data['_latitude_'])}
-# ...
-# es.index(index="geodata", doc_type="doc", body=es_entries)
-
-
-
 
 def ingest_events(page, per_page=1000):
-    # api call
     response = requests.get('https://api.seatgeek.com/2/events?per_page=%s&page=%s' % (per_page, page), auth=(os.environ["GS_CLIENT_ID"], os.environ["GS_CLIENT_SECRET"]))
-    # write to elastic search
     response_json = response.json()
+    
     print(response_json['meta'])
     print(len(response_json["events"]))
+    
     if not response_json["events"]:
         raise StopIteration
+        
     for event in response_json["events"]:
-        location = event["venue"]["location"]
-        l = {"location": location}
-        event.update(l)
+        event["location"] = event["venue"]["location"]
         es.index(index='sg2', doc_type='seat_geek', id=event["id"], body=event)
 
 
